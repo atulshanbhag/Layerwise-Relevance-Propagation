8f6e4cda279c94cfcd5f9b8584d0019e60caea12
Atul Shanbhag <shanbhag1996@gmail.com>
2018-03-03 14:55:58 +0530

    Comments to utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import numpy as np
 
 DATA_PATH = './mnist.pkl.gz'
 
+# Load MNIST encoded as One-Hot labels
 def load_data():
 	try:
 		with gzip.open(DATA_PATH, 'rb') as f:
@@ -16,6 +17,7 @@ def load_data():
 	x_val, y_val = val_set
 	x_test, y_test = test_set
 
+	# One-Hot labelling
 	I = np.eye(10)
 	y_train = I[y_train]
 	y_val = I[y_val]
