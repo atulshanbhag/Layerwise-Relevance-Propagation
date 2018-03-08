7fb8d782d5aaaade01150ac3f78af149f2d9217c
Atul Shanbhag <shanbhag1996@gmail.com>
2018-03-08 20:14:36 +0530

    Added Trainer class, with validation accuracy

--- train.py
+++ train.py
@@ -7,65 +7,86 @@ chkpt = './logs/model.ckpt'
 n_epochs = 15
 batch_size = 100
 
-mnist = MNISTLoader()
-mnist()
-x_train, y_train = mnist.train
-x_validation, y_validation = mnist.validation
+class Trainer:
 
-with tf.name_scope('MNIST_CNN'):
-	model = MNIST_CNN()
-
-	X = tf.placeholder(tf.float32, [None, 784], name='X')
-	y = tf.placeholder(tf.float32, [None, 10], name='y')
-
-	params, logits = model(X)
-
-	tf.add_to_collection('DTD', X)
-	for p in params:
-		tf.add_to_collection('DTD', p)
-
-	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=y))
-	optimizer = tf.train.AdamOptimizer().minimize(cost, var_list=model.params)
-
-	preds = tf.equal(tf.argmax(logits, axis=1), tf.argmax(y, axis=1))
-	accuracy = tf.reduce_mean(tf.cast(preds, tf.float32))
-
-cost_summary = tf.summary.scalar(name='Cost', tensor=cost)
-accuracy_summary = tf.summary.scalar(name='Accuracy', tensor=accuracy)
-
-summary = tf.summary.merge_all()
-
-with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())
-
-	saver = tf.train.Saver()
-	file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
-
-	train_batch = DataGenerator(x_train, y_train, batch_size)
-
-	print('Training...')
-	for epoch in range(n_epochs):
-		n_batches = x_train.shape[0] // batch_size
-		if x_train.shape[0] % batch_size != 0:
-			n_batches += 1
-		avg_cost = 0
-		avg_accuracy = 0
-
-		for batch in range(n_batches):
-			 x_batch, y_batch = next(train_batch)
-			 _, batch_cost, batch_accuracy, summ = sess.run([optimizer, cost, accuracy, summary], 
-			 																								feed_dict={X: x_batch, y: y_batch})
-			 avg_cost += batch_cost
-			 avg_accuracy += batch_accuracy
-			 file_writer.add_summary(summ, epoch * n_batches + batch)
-
-			 completion = batch / n_batches
-			 print_str = '|' + int(completion * 20) * '#' + (19 - int(completion * 20))  * ' ' + '|'
-			 print('\rEpoch {0:>3} {1} {2:3.0f}% Cost {3:6.4f} Accuracy {4:6.4f}'.format('#' + str(epoch + 1), 
-							print_str, completion * 100, avg_cost / (batch + 1), avg_accuracy / (batch + 1)), end='')
+	def __init__(self):
+		self.dataloader = MNISTLoader()
+		self.dataloader()
 		
-		validation_accuracy = sess.run([accuracy, ], feed_dict={X: x_validation, y: y_validation})
-		print(' Validation Accuracy {0:6.4f}'.format(validation_accuracy))
+		self.x_train, self.y_train = self.dataloader.train
+		self.x_validation, self.y_validation = self.dataloader.validation
 
-		saver.save(sess, chkpt)
+		with tf.variable_scope('MNIST_CNN'):
+			self.model = MNIST_CNN()
 
+			self.X = tf.placeholder(tf.float32, [None, 784], name='X')
+			self.y = tf.placeholder(tf.float32, [None, 10], name='y')
+
+			self.params, self.logits = self.model(self.X)
+
+			tf.add_to_collection('DeepTaylorDecomposition', self.X)
+			for p in self.params:
+				tf.add_to_collection('DeepTaylorDecomposition', p)
+
+			self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.y))
+			self.optimizer = tf.train.AdamOptimizer().minimize(self.cost, var_list=self.model.params)
+
+			self.preds = tf.equal(tf.argmax(self.logits, axis=1), tf.argmax(self.y, axis=1))
+			self.accuracy = tf.reduce_mean(tf.cast(self.preds, tf.float32))
+
+		self.cost_summary = tf.summary.scalar(name='Cost', tensor=self.cost)
+		self.accuracy_summary = tf.summary.scalar(name='Accuracy', tensor=self.accuracy)
+
+		self.summary = tf.summary.merge_all()
+
+	def run(self):
+		with tf.Session() as sess:
+			sess.run(tf.global_variables_initializer())
+
+			self.saver = tf.train.Saver()
+			self.file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
+			self.train_batch = DataGenerator(self.x_train, self.y_train, batch_size)
+			self.validation_batch = DataGenerator(self.x_validation, self.y_validation, batch_size)
+
+			for epoch in range(n_epochs):
+				self.train(sess, epoch)
+				self.validate(sess)
+				self.saver.save(sess, chkpt)
+
+	def train(self, sess, epoch):
+		n_batches = self.x_train.shape[0] // batch_size
+		if self.x_train.shape[0] % batch_size != 0:
+			n_batches += 1
+
+		avg_cost = 0
+		avg_accuracy = 0	
+		for batch in range(n_batches):
+			x_batch, y_batch = next(self.train_batch)
+			_, batch_cost, batch_accuracy, summ = sess.run([self.optimizer, self.cost, self.accuracy, self.summary], 
+																											feed_dict={self.X: x_batch, self.y: y_batch})
+			avg_cost += batch_cost
+			avg_accuracy += batch_accuracy
+			self.file_writer.add_summary(summ, epoch * n_batches + batch)
+
+			completion = batch / n_batches
+			print_str = '|' + int(completion * 20) * '#' + (19 - int(completion * 20))  * ' ' + '|'
+			print('\rEpoch {0:>3} {1} {2:3.0f}% Cost {3:6.4f} Accuracy {4:6.4f}'.format('#' + str(epoch + 1), 
+				print_str, completion * 100, avg_cost / (batch + 1), avg_accuracy / (batch + 1)), end='')
+		print(end=' ')
+
+	def validate(self, sess):
+		n_batches = self.x_validation.shape[0] // batch_size
+		if self.x_validation.shape[0] % batch_size != 0:
+			n_batches += 1
+		
+		avg_accuracy = 0
+		for batch in range(n_batches):
+			x_batch, y_batch = next(self.validation_batch)
+			avg_accuracy += sess.run([self.accuracy, ], feed_dict={self.X: x_batch, self.y: y_batch})[0]
+
+		avg_accuracy /= n_batches
+		print('Validation Accuracy {0:6.4f}'.format(avg_accuracy))
+
+if __name__ == '__main__':
+	Trainer().run()
